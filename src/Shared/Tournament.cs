b79cedaf649441bb428f65a279e7f1842a338c95
Janne Schyffert <Janne.schyffert@me.com>
2022-05-01 19:32:20 +0200

    Test and cleanup tournament structure
    Fully tested! Should work for everything now

--- src/Shared/Tournament.cs
+++ src/Shared/Tournament.cs
@@ -7,6 +7,167 @@ namespace best_song.Shared
 {
     public class Tournament<T>
     {
+        private int _currentNodeIndex;
+
+        private List<Node> _topLevelNodes;
+
+        public Tournament(List<T> entrys)
+        {
+            OrganizeNewTopLevel(entrys);
+        }
+
+        public Tournament(T[] entrys) : this(entrys.ToList())
+        {
+        } // Alternate ctor for arrays
+
+        public int TopLevelSize => _topLevelNodes.Count;
+
+        public T[] CurrentMatchUp
+        {
+            get
+            {
+                var currentNode = _topLevelNodes[_currentNodeIndex];
+                if (currentNode.ChildCount == 1 || currentNode.ChildCount == 0)
+                {
+                    if (_currentNodeIndex == TopLevelSize - 1)
+                    {
+                        OrganizeNewTopLevel();
+                        return CurrentMatchUp;
+                    }
+
+                    return _topLevelNodes[++_currentNodeIndex].ToArray();
+                }
+
+                return currentNode.ToArray();
+            }
+        }
+
+        public bool Win(T value)
+        {
+            var currentNode = _topLevelNodes[_currentNodeIndex];
+            switch (currentNode.ChildCount)
+            {
+                case 0:
+                    if (_currentNodeIndex == TopLevelSize - 1)
+                    {
+                        OrganizeNewTopLevel();
+                        return false;
+                    }
+
+                    _currentNodeIndex++;
+                    return false;
+                case 1:
+                    currentNode.Value = currentNode.ToArray()[0];
+                    if (_currentNodeIndex == TopLevelSize - 1)
+                    {
+                        OrganizeNewTopLevel();
+                        return false;
+                    }
+
+                    _currentNodeIndex++;
+                    return false;
+            }
+
+            if (!currentNode.Left.Value.Equals(value) && !currentNode.Right.Value.Equals(value))
+                throw new ArgumentException("Value could not be found in current match up");
+
+            if (_currentNodeIndex == _topLevelNodes.Count - 1) // Final match up of this level
+            {
+                if (TopLevelSize == 1) // Final match up of tournament
+                {
+                    _topLevelNodes[0].Value = value;
+                    return true;
+                }
+
+                currentNode.Value = value;
+                OrganizeNewTopLevel();
+                return false;
+            }
+
+            currentNode.Value = value;
+            _currentNodeIndex++;
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return _topLevelNodes.ToString() ?? throw new InvalidOperationException();
+        }
+
+        // Helper methods:
+        private void Shuffle(List<T> list)
+        {
+            var rng = new Random();
+            var n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                var k = rng.Next(n + 1);
+                (list[k], list[n]) = (list[n], list[k]);
+            }
+        }
+
+        private void Shuffle(List<Node> list)
+        {
+            var rng = new Random();
+            var n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                var k = rng.Next(n + 1);
+                (list[k], list[n]) = (list[n], list[k]);
+            }
+        }
+
+        private void OrganizeNewTopLevel(List<T> entries)
+        {
+            if (_currentNodeIndex != 0 || entries.Count == 0) throw new ArgumentException();
+
+            Shuffle(entries.ToList());
+            List<Node> newTopLevel = new();
+
+            if (entries.Count % 2 != 0)
+            {
+                for (var i = 0; i < entries.Count - 1; i++) newTopLevel.Add(new Node(entries[i++], entries[i]));
+
+                newTopLevel.Add(new Node(entries[^1]));
+            }
+            else
+            {
+                for (var i = 0; i < entries.Count; i++) newTopLevel.Add(new Node(entries[i++], entries[i]));
+            }
+
+            _topLevelNodes = newTopLevel;
+            _currentNodeIndex = 0;
+        }
+
+        private void OrganizeNewTopLevel()
+        {
+            if (_currentNodeIndex != _topLevelNodes.Count - 1)
+                throw new InvalidOperationException(
+                    "Tried to generate new level of nodes with current level unfinished " +
+                    "current index: {currentNodeIndex}");
+
+            Shuffle(_topLevelNodes);
+            List<Node> newTopLevel = new();
+
+            if (_topLevelNodes.Count % 2 != 0)
+            {
+                for (var i = 0; i < _topLevelNodes.Count - 1; i++)
+                    newTopLevel.Add(new Node(_topLevelNodes[i++], _topLevelNodes[i]));
+
+                newTopLevel.Add(new Node(_topLevelNodes[^1], null));
+            }
+            else
+            {
+                for (var i = 0; i < _topLevelNodes.Count; i++)
+                    newTopLevel.Add(new Node(_topLevelNodes[i++], _topLevelNodes[i]));
+            }
+
+            _topLevelNodes = newTopLevel;
+            _currentNodeIndex = 0;
+        }
+
         internal class Node
         {
             internal readonly Node? Left;
@@ -16,8 +177,8 @@ namespace best_song.Shared
             // Constructors, one bottom-level and one top-level
             internal Node(T leftValue, T rightValue)
             {
-                Left = new(leftValue);
-                Right = new(rightValue);
+                Left = new Node(leftValue);
+                Right = new Node(rightValue);
                 Value = default;
             }
 
@@ -25,150 +186,35 @@ namespace best_song.Shared
             {
                 Left = null;
                 Right = null;
-                this.Value = value;
+                Value = value;
             }
 
             internal Node(Node? left, Node? right)
             {
-                this.Left = left;
-                this.Right = right;
+                Left = left;
+                Right = right;
                 Value = default;
+            }
+
+            internal int ChildCount
+            {
+                get
+                {
+                    if ((Left == null) ^ (Right == null)) return 1;
+                    if (Left != null && Right != null) return 2;
+                    return 0;
+                }
             }
 
             internal T[] ToArray()
             {
-                return new T[] { Left.Value, Right.Value };
+                if (Right != null && Left != null) return new[] { Left.Value, Right.Value };
+                if (Right == null && Left == null) return new[] { Value };
+                if (Right != null)
+                    return new[] { Right.Value };
+                if (Left != null) return new[] { Left.Value };
+                return Array.Empty<T>();
             }
-        }
-
-        private List<Node> topLevelNodes;
-        private int currentNodeIndex;
-        public int TopLevelSize { get => topLevelNodes.Count; }
-        public T[] currentMatchup
-        {
-            get => topLevelNodes[currentNodeIndex].ToArray();
-        }
-
-        public Tournament(List<T> entrys)
-        {
-            organizeNewTopLevel(entrys);
-        }
-
-        public Tournament(T[] entrys) : this(entrys.ToList()) { } // Alternate ctor for arrays
-
-        public bool win(T value)
-        {
-            var currentNode = topLevelNodes[currentNodeIndex];
-            if (!currentNode.Left.Value.Equals(value) && !currentNode.Right.Value.Equals(value))
-            {
-                throw new ArgumentException("Value could not be found in current matchup");
-            }
-
-            if (currentNodeIndex + 1 == topLevelNodes.Count - 1) // Final matchup of this level
-            {
-                if(TopLevelSize == 1) // Final matchup of tournament
-                {
-                    topLevelNodes[0].Value = value;
-                    return true;
-                }
-
-                currentNode.Value = value;
-                organizeNewTopLevel();
-                return false;
-            }
-            currentNode.Value = value;
-            currentNodeIndex++;
-            return false;
-        }
-
-        // Helper methods:
-        private void shuffle(List<T> list)
-        {
-            var rng = new Random();
-            int n = list.Count;
-            while (n > 1)
-            {
-                n--;
-                int k = rng.Next(n + 1);
-                var value = list[k];
-                list[k] = list[n];
-                list[n] = value;
-            }
-        }
-
-        private void shuffle(List<Node> list)
-        {
-            var rng = new Random();
-            int n = list.Count;
-            while (n > 1)
-            {
-                n--;
-                int k = rng.Next(n + 1);
-                var value = list[k];
-                list[k] = list[n];
-                list[n] = value;
-            }
-        }
-
-        private void organizeNewTopLevel(List<T> entrys)
-        {
-            if (currentNodeIndex != 0 || entrys.Count == 0)
-            {
-                throw new ArgumentException();
-            }
-
-            shuffle(entrys.ToList());
-            List<Node> newTopLevel = new();
-
-            if (entrys.Count() % 2 != 0)
-            {
-                for (int i = 0; i < entrys.Count - 1; i++)
-                {
-                    newTopLevel.Add(new(entrys[i++], entrys[i]));
-                }
-                newTopLevel.Add(new(entrys[^0]));
-            }
-            else
-            {
-                for (int i = 0; i < entrys.Count; i++)
-                {
-                    newTopLevel.Add(new(entrys[i++], entrys[i]));
-                }
-            }
-
-            topLevelNodes = newTopLevel;
-            currentNodeIndex = 0;
-        }
-
-        private void organizeNewTopLevel()
-        {
-            if (currentNodeIndex != topLevelNodes.Count - 1)
-            {
-                throw new SystemException($"Tried to generate new level of nodes with current level unfinished " +
-                    "current index: {currentNodeIndex}");
-            }
-
-            shuffle(topLevelNodes);
-            List<Node> newTopLevel = new();
-
-            if (topLevelNodes.Count() % 2 != 0)
-            {
-                for (int i = 0; i < topLevelNodes.Count - 1; i++)
-                {
-                    newTopLevel.Add(new(topLevelNodes[i++], topLevelNodes[i]));
-                }
-                newTopLevel.Add(new(topLevelNodes[^0], null));
-            }
-            else
-            {
-                for (int i = 0; i < topLevelNodes.Count; i++)
-                {
-                    newTopLevel.Add(new(topLevelNodes[i++], topLevelNodes[i]));
-                }
-            }
-
-            topLevelNodes = newTopLevel;
-            currentNodeIndex = 0;
         }
     }
 }
--- tests/TournamentTests.cs
+++ tests/TournamentTests.cs
@@ -1,20 +1,37 @@
-using Xunit;
-using Bunit;
-using best_song;
-using System.Collections.Generic;
 using System;
+using best_song.Shared;
+using Xunit;
+
 public class TournamentTests
 {
-    TournamentTests()
+    private readonly Tournament<int> evenTrn;
+    private readonly Tournament<int> unevenTrn;
+
+    public TournamentTests()
     {
         int[] unevenEntry = { 1, 2, 3, 4, 5 };
         int[] evenEntry = { 1, 2, 3, 4 };
-        int[] emptyEntry = Array.Empty<int>();
+        var emptyEntry = Array.Empty<int>();
+        unevenTrn = new Tournament<int>(unevenEntry);
+        evenTrn = new Tournament<int>(evenEntry);
     }
 
     [Fact]
     public void evenEntryIsFinishedAfter3Rounds()
     {
-        
+        evenTrn.Win(evenTrn.CurrentMatchUp[0]);
+        evenTrn.Win(evenTrn.CurrentMatchUp[0]);
+        Assert.True(evenTrn.Win(evenTrn.CurrentMatchUp[0]));
+    }
+
+    [Fact]
+    public void unevenEntryIsFinishedAfter4Rounds()
+    {
+        // Arrange
+        var actual = false;
+        // Act
+        for (var i = 0; i < 4; i++) actual = unevenTrn.Win(unevenTrn.CurrentMatchUp[0]);
+        // Assert
+        Assert.True(actual);
     }
 }
