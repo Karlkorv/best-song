e03c6c856d7c2a365b818773bc6259dbbc74df6a
Janne Schyffert <Janne.schyffert@me.com>
2022-05-19 11:19:41 +0200

    Add tests

--- tests/TournamentTests.cs
+++ tests/TournamentTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using best_song.Data;
 using Xunit;
 
@@ -33,5 +34,51 @@ public class TournamentTests
         for (var i = 0; i < 4; i++) actual = unevenTrn.Win(unevenTrn.CurrentMatchUp[0]);
         // Assert
         Assert.True(actual);
+    }
+
+    [Fact]
+    public void MatchUpsLeftIsCorrect()
+    {
+        var expected = 3;
+        var trnFinished = false;
+
+        while (!trnFinished)
+        {
+            Assert.Equal(expected, evenTrn.MatchUpsLeft);
+            expected--;
+            trnFinished = evenTrn.Win(evenTrn.CurrentMatchUp[0]);
+        }
+    }
+
+    [Fact]
+    public void MatchUpsLeftIsCorrectForLargerTrn()
+    {
+        List<int> largerList = new();
+        for (var i = 0; i < 16; i++) largerList.Add(i);
+
+        Tournament<int> largerTrn = new(largerList);
+
+        var expected = 8 + 4 + 2 + 1;
+        var trnFinished = false;
+
+        while (!trnFinished)
+        {
+            Assert.Equal(expected, largerTrn.MatchUpsLeft);
+            expected--;
+            trnFinished = largerTrn.Win(largerTrn.CurrentMatchUp[0]);
+        }
+    }
+
+    [Fact]
+    public void MatchUpsLeftWorksForUnevenTrn()
+    {
+        var expected = 4;
+        var trnFinished = false;
+        while (!trnFinished)
+        {
+            Assert.Equal(expected, unevenTrn.MatchUpsLeft);
+            trnFinished = unevenTrn.Win(unevenTrn.CurrentMatchUp[0]);
+            expected--;
+        }
     }
 }
