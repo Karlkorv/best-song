095d75ec23b393da4e2f7fe4c5ae582d914c9007
Janne Schyffert <Janne.schyffert@me.com>
2022-05-19 10:57:55 +0200

    Add MatchupsLeft property to Tournament.cs

--- src/Data/Tournament.cs
+++ src/Data/Tournament.cs
@@ -45,6 +45,8 @@ public class Tournament<T>
 
     public T? Winner { get; private set; }
 
+    public int MatchUpsLeft => GetMatchUpsLeft(TopLevelSize);
+
     /// <summary>
     ///     Current tournament match up that needs to be resolved, one of the two values has to win.
     /// </summary>
@@ -215,6 +217,13 @@ public class Tournament<T>
 
         _currentNodeIndex = 0;
         return newTopLevel;
+    }
+
+    private int GetMatchUpsLeft(int curTopSize)
+    {
+        if (curTopSize <= 1) return 0;
+        if (curTopSize == TopLevelSize) return curTopSize - _currentNodeIndex - 1 + GetMatchUpsLeft(curTopSize / 2);
+        return curTopSize + GetMatchUpsLeft(curTopSize / 2);
     }
 
     internal class Node
